8fd9686102fde33855c9c0f26944a9954c631924
Hope Halfbreed <boldyrevanadezhda@gmail.com>
2024-08-23 12:18:18 +0700

    fix: Поведение боковой панели

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,14 +21,25 @@
     </q-header>
 
     <q-drawer
+      v-model="drawerModelValue"
       show-if-above
       bordered
-      :mini="drawerIsOpen"
+      v-bind="drawerProps"
       class="sidebar flex"
     >
       <q-list class="sidebar__list">
-        <q-item-label header>
-          Essential Links
+        <q-item-label
+          header
+          class="flex justify-between items-center"
+        >
+          <span>Essential Links</span>
+          <q-btn
+            text-color="gray"
+            :title="$t('layouts.buttons.closeDrawer')"
+            icon="close"
+            size="sm"
+            @click="toggleLeftDrawer"
+          />
         </q-item-label>
 
         <EssentialLink
@@ -56,15 +67,18 @@
   </q-layout>
 </template>
 
-<script
-  setup
-  lang="ts"
->
-import { ref } from 'vue';
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import EssentialLink from 'components/EssentialLink.vue';
+import { useQuasar } from 'quasar';
+
+interface IDrawerProps {
+  mini?: boolean;
+}
 
 const { t } = useI18n();
+const $q = useQuasar();
 
 const essentialLinks = [
   {
@@ -75,17 +89,55 @@ const essentialLinks = [
   }
 ];
 
-const drawerIsOpen = ref(false);
+const drawerModelValue = ref<boolean>(false);
+const drawerIsOpen = ref<boolean>(false);
 
-function toggleLeftDrawer() {
-  drawerIsOpen.value = !drawerIsOpen.value
+const isDesktopScreen = computed((): boolean => (
+  !$q.screen.lt.md
+));
+
+const drawerProps = computed((): IDrawerProps => {
+  console.log('isDesktopScreen.value', isDesktopScreen.value);
+
+  let mini = isDesktopScreen.value
+    ? !drawerIsOpen.value
+    : false;
+
+  return {
+    mini
+  };
+})
+
+const init = () => {
+  setDrawerMode();
 }
+
+const setDrawerMode = () => {
+  if (isDesktopScreen.value) {
+    drawerIsOpen.value = false;
+    drawerModelValue.value = true;
+  } else {
+    drawerIsOpen.value = false;
+    drawerModelValue.value = false;
+  }
+}
+
+const toggleLeftDrawer = () => {
+  if (isDesktopScreen.value) {
+    drawerIsOpen.value = !drawerIsOpen.value;
+    drawerModelValue.value = true;
+  } else {
+    drawerModelValue.value = !drawerModelValue.value;
+    drawerIsOpen.value = false;
+  }
+}
+
+onMounted(() => {
+  init();
+})
 </script>
 
-<style
-  lang="scss"
-  scoped
->
+<style lang="scss" scoped>
 .sidebar__list {
   width: 100%;
 }
