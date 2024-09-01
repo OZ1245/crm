cbd4254037b6a0acb23747ab0376856260016e66
Hope Halfbreed <boldyrevanadezhda@gmail.com>
2024-09-01 20:51:38 +0700

    fix: Чтение языка во время загрузки приложения

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useAccountStore } from './stores/account';
+import { useI18n } from 'vue-i18n';
 
+const accountStore = useAccountStore();
+const { locale } = useI18n();
+
+const accountLanguage = computed((): string => (
+  accountStore.getAccountPreferences?.language || ''
+))
+
+locale.value = accountLanguage.value;
 </script>
